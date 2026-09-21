a66cb84906729171542478ff3ab9294464726279
SakshiChougule2007 <sakshimaheshchougule9270312843@gmail.com>
2026-09-22 02:08:37 +0530

    Use local MySQL database for backend
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    .git/COMMIT_EDITMSG [unix] (02:14 22/09/2026)                                                                               1,1 All
    "~/StudioProjects/kabadiwala/.git/COMMIT_EDITMSG" [unix] 33L, 770BUse local MySQL database for backend

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,14 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = "mysql+pymysql://root:sakshi@localhost/kabadiconnect"
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "postgresql://neondb_owner:npg_zWAx4eXwb7BR@ep-plain-surf-b4ljg6lo-pooler.c-6.us-east-2.aws.neon.tech/neondb?sslmode=require"
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
-# Standard engine creation for Neon PostgreSQL
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
